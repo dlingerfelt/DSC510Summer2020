03043344a7f031aca8eb3dac7f8fd66c486b4a45
MichaelHotaling <hotalimj@lemoyne.edu>
2020-06-25 10:58:30 -0400

    Assignment 4.1 Created a seperate function for printing the receipt Created a loop in the input_cable_length() function until the user inputs a correct numerical value

--- HOTALING_DSC510/cablePriceFunction.py
+++ HOTALING_DSC510/cablePriceFunction.py
@@ -2,7 +2,7 @@
 # Week 4
 # Programming Assignment Week 4
 # Author: Michael Hotaling
-# 06/24/2020
+# 06/25/2020
 
 
 def price_calculation(feet):
@@ -21,7 +21,7 @@ def price_calculation(feet):
 
 def regular_price():
     # We can declare the standard price here so we can call it later when checking how much money the user saved
-    # This can be helpful if the standard price ever changes
+    # This can be helpful if the standard price ever changes or if we need to raise prices in the future
     return 0.87
 
 
@@ -33,14 +33,28 @@ def bill(feet, price):
 
 def input_cable_length():
     # Create the function to ask the user for how many feet of fiber they need
-    # There is error handling, but if the input triggers an error, the program is terminated
+    # There is error handling, so if the user inputs an incorrect value, the function will loop
     try:
         length = float(input('How many feet of fiber optic cable is required?: '))
         return length
     except ValueError:
         print("Oops! Please put in a numerical value. eg. 55, 3.14, 9999")
         print("Please try again!")
-        exit()
+        return input_cable_length()
+
+
+def print_receipt(company_name, cable_length, cable_price, final_cost):
+    # Printing the receipt
+    # This program will intake 4 variables, the company name, the cable length, the cable price, and the final cost
+    print("--" + company_name + " Cable Company--")
+    print("--Fiber Optic Receipt--")
+    print("--Feet of Cable: " + str(cable_length) + "--")
+    print("--Price per Foot: " + (f'${cable_price:,.2f}'.replace('$-', '-$')) + "--")
+    print("--Total Cost: " + (f'${final_cost:,.2f}'.replace('$-', '-$')) + "--")
+    if cable_price != 0.87 and cable_length > 0:
+        print("--You saved " + (f'${cable_length * regular_price() - final_cost :,.2f}'.replace('$-', '-$')) +
+              " with our discount!--")
+    print("--Please come again soon!--")
 
 
 def main():
@@ -52,10 +66,8 @@ def main():
     print("Okay! You are from " + company + ". That's great to hear!")
 
     # Use the input_cable_length() function to ask the user how many feet of cable they need
+    # We will then call all the functions we need to calculate their bill
     fiber_length = input_cable_length()
-
-    # We will check for the price per length of cable here
-    # If the user requests more than a certain amount of cable, they will get a discount
     fiber_price = price_calculation(fiber_length)
     total_cost = bill(fiber_length, fiber_price)
 
@@ -63,7 +75,7 @@ def main():
     if fiber_price != 0.87 and fiber_length > 0:
         print("Good news! You're eligible for our bulk discount!")
 
-    # We are using the correct formatting now for the price format
+    # Here are some statements if the user inputs negative values or a 0
     if fiber_length < 0:
         print('Oh! Okay! You need to return ' + str(-fiber_length) + ' feet of fiber cable. We will credit you ' +
               f'${-total_cost:,.2f}'.replace('$-', '-$'))
@@ -76,17 +88,9 @@ def main():
     # Some print statements to create some room between the chat and the receipt
     print()
 
-    # Printing the receipt
-    print("--" + company + " Cable Company--")
-    print("--Fiber Optic Receipt--")
-    print("--Feet of Cable: " + str(fiber_length) + "--")
-    print("--Price per Foot: " + (f'${fiber_price:,.2f}'.replace('$-', '-$')) + "--")
-    print("--Total Cost: " + (f'${total_cost:,.2f}'.replace('$-', '-$')) + "--")
+    # Calling the print function
+    print_receipt(company, fiber_length, fiber_price, total_cost)
 
-    if fiber_price != 0.87 and fiber_length > 0:
-        print("--You saved " + (f'${fiber_length * regular_price() - total_cost :,.2f}'.replace('$-', '-$')) +
-              " with our discount!--")
-    print("--Please come again soon!--")
 
 if __name__ == "__main__":
     main()
