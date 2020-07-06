6d9e1c16c8aad2c11b1418c2dfd77176daaf3f0d
Michael Hotaling <37930981+MichaelHotaling@users.noreply.github.com>
2020-07-06 13:43:18 -0400

    Assignment 7.1
    Added comments

--- HOTALING_DSC510/wordCounter.py
+++ HOTALING_DSC510/wordCounter.py
@@ -1,37 +1,46 @@
 # DSC 510
-# Week 6
-# Programming Assignment Week 6
+# Week 7
+# Programming Assignment Week 7
 # Author: Michael Hotaling
 # 07/06/2020
 
 import string
-import re
 
-def process_add_words(words, word_dictionary):
+
+def add_words(words, word_dictionary):
+    # We can use an iterative function to add each word to the library
+    # If the word doesn't yet exist in the library, we can set the value equal to 1
     for word in words:
-        if word in word_dictionary:
-            word_dictionary[word] += 1
-        else:
-            word_dictionary[word] = 1
+        if word != "":
+            if word in word_dictionary:
+                word_dictionary[word] += 1
+            else:
+                word_dictionary[word] = 1
 
 
 def process_line(line, word_dictionary):
+    # We will first call strip to remove the leading and trailing spaces in the string
     line = line.strip()
+    # Next, we convert all the characters to lower case.
     line = line.lower()
-    line = line.translate((line.maketrans("", "", string.punctuation)))
+    # This ia bit more complicated. We need to remove special characters from our input.
+    # We can use translate to covert the special characters to spaces
+    line = line.translate(line.maketrans("", "", string.punctuation))
+    # Finally, we can split all the words in each line using spaces as the delimiter
     words = line.split(" ")
-    process_add_words(words, word_dictionary)
+    # We can then call the process_add_words function to add the words to the dictionary
+    add_words(words, word_dictionary)
 
 
-def format_print(word_dictionary):
+def pretty_print(word_dictionary):
     print()
-    print("The number of words in the dictionary is " + str(len(word_dictionary)))
-    print()
-    print("{:<25} {:10} ".format("Word", " Count"))
-    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("Length of the dictionary: " + str(len(word_dictionary)))
+    print("-" * 34)
+    print("{:<26} {:10} ".format("|Word", " Count|"))
+    print("|" + ("-" * 32) + "|")
     for key in sorted(word_dictionary, key=word_dictionary.get, reverse=True):
-        if key.strip() != "": # Need this statement or we get spaces back. (not sure why)
-            print("{:<15} {:15} ".format(key, word_dictionary[key]))
+        print("|{:<15} {:15} |".format(key, word_dictionary[key]))
+    print("-" * 34)
 
 
 def main():
@@ -40,7 +49,7 @@ def main():
     for line in gba_file:
         process_line(line, word_dictionary)
     gba_file.close()
-    format_print(word_dictionary)
+    pretty_print(word_dictionary)
 
 
 if __name__ == "__main__":
