207f779be587882a41dc37b2214aebb00a0088e6
Michael Hotaling <37930981+MichaelHotaling@users.noreply.github.com>
2020-07-07 07:42:30 -0400

    Assignment 6.1

--- HOTALING_DSC510/temperatureRecorder.py
+++ HOTALING_DSC510/temperatureRecorder.py
@@ -5,6 +5,20 @@
 # 07/06/2020
 
 
+# I made several math functions in week 4 so I thought I might use them for this homework assignment :)
+
+
+def mean(numbers):
+    # This one is pretty easy. We can just create a variable named summer and set it equal to 0.
+    # For every element in the list, we can add the value to the summer and once we reach the end of the list
+    # we can divide the summer by the number of elements.
+    summer = 0
+    for i in numbers:
+        summer += i
+    average = (summer / len(numbers))
+    return average
+
+
 def median(numbers):
     # Added this for fun
     # This function will find the median value in a list
@@ -21,6 +35,47 @@ def median(numbers):
     else:
         med = numbers[(length // 2)]
         return med
+
+
+def variance(numbers):
+    # Variance can be understood as a measurement of the spread between numbers in an array of numbers
+    # Simplified, variance can be though of as how far each value in a set is from the mean of the set and,
+    # therefore, from every other number in the set.
+    # We can create a new list and fill it with the calculated values. Once that's done, we can take the mean
+    var_list = []
+    mu = mean(numbers)
+    for i in numbers:
+        var_list.append((i - mu) ** 2)
+    return mean(var_list)
+
+
+def square_root(number):
+    # https://www.cse.wustl.edu/~cytron/101Pages/f08/Notes/SquareRoot/sqrt.html
+    # https://en.wikipedia.org/wiki/Methods_of_computing_square_roots
+    # https://en.wikipedia.org/wiki/Newton%27s_method
+    # This one was actually pretty tricky. We could just use the built in exponential function ** and use 0.5 as the
+    # exponent, but that wouldn't be any fun. Instead, we can create a recursive function to approximate the square
+    # root of a number. This is known as Newtons Method
+    start = 0
+    end = number
+    guess = 0
+    threshold = 0.0000000000001
+
+    while end - start > threshold:
+        guess = (start + end) / 2
+        m_square = guess * guess
+        if abs(m_square - number) <= threshold:
+            return guess
+        elif m_square < number:
+            start = guess
+        else:
+            end = guess
+    return guess
+
+
+def standard_deviation(numbers):
+    # Now that we have defined square root and variance, all we need to do here is use both to find the square root.
+    return square_root(variance(numbers))
 
 
 def temp_recorder(temp):
@@ -51,9 +106,10 @@ def pretty_format(temp_list):
         print("The maximum temperature is " + str(max(temp_list)))
         print("The minimum temperature is " + str(min(temp_list)))
         print("The number of measurement inputs is " + str(len(temp_list)))
-        print("The average value of measurement inputs is " + str(sum(temp_list) / len(temp_list)))
+        print("The average value of measurement inputs is " + str(mean(temp_list)))
         print("The median value of the measurement inputs is " + str(median(temp_list)))
-
+        print("The variance of the measurement inputs is " + str(variance(temp_list)))
+        print("The standard deviation of the measurement inputs is " + str(standard_deviation(temp_list)))
     except ValueError:
         print("Something went wrong!")
         print("Did you forget to input a temperature? ")
@@ -76,11 +132,11 @@ def main():
         pretty_format(temps)
         print()
         again = input("Would you like to run the program again? [y/n]:  ")
-        if again.lower()[0] != "y":
+        if again.lower() != "y":
             break
         # Here, we give the user the opportunity to either continue with their list, or start fresh.
         user_continue = input("Would you like to continue where you left off? [y/n]: ")
-        if user_continue.lower()[0] != "y":
+        if user_continue.lower() != "y":
             # This line resets the temp values
             temps = []
             print("List erased! Starting over!")
