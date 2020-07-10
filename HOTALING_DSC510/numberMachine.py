9fa4188b56499789c3f08591bf42bc1a3c28083d
Michael Hotaling <37930981+MichaelHotaling@users.noreply.github.com>
2020-07-10 12:10:51 -0400

    Assignment 6.2
    Changed the formula into a function

--- HOTALING_DSC510/numberMachine.py
+++ HOTALING_DSC510/numberMachine.py
@@ -1,22 +1,23 @@
 # Mike's Stupid Number Machine
 
 
-# I added the teen numbers to the singles list. This is useful for numbers between 1-19, but if we ever want to increase
-# range of this program, we will run into issues. ie 116.
-singles_and_teens = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven",
-                     "twelve",
-                     "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen",""]
+def num_to_word(i):
+    # I added the teen numbers to the singles list. This is useful for numbers between 1-19, but if we ever want to 
+    # increase range of this program, we will run into issues. ie 116. 
+    singles_and_teens = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven",
+                         "twelve",
+                         "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", ""]
 
-# Second digit lists
-tens = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+    # Second digit lists
+    tens = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
 
-# We don't need a hundred list technically. We should be able to check the length of the number. If it is 3, we can pull
-# The first digit, index the singles list, and add "hundred" to the string. The rest is self explanatory.
+    # We don't need a hundred list technically. We should be able to check the length of the number. If it is 3, 
+    # we can pull The first digit, index the singles list, and add "hundred" to the string. The rest is self 
+    # explanatory. 
 
-hundreds_and_beyond = ["", "", "hundred", "thousand", "thousand", " hundred thousand", "million"]
+    # Futuristic
+    hundreds_and_beyond = ["", "", "hundred", "thousand", "thousand", " hundred thousand", "million"]
 
-# Testing the program
-for i in range(1, 1000):
     if i <= 19:
         print(singles_and_teens[i])
     elif 20 <= i <= 99:
@@ -26,5 +27,12 @@ for i in range(1, 1000):
     elif len(str(i)) == 3 and str(i)[1] == "0" or str(i)[1] == "1":
         print(str(singles_and_teens[int(str(i)[0])]) + " hundred and " + str(singles_and_teens[int(str(i)[1:])]))
     else:
-        print(str(singles_and_teens[int(str(i)[0])]) + " hundred and " + str(tens[int(str(i)[1]) - 2]) + " " + str(singles_and_teens[int(str(i)[2])]))
+        print(str(singles_and_teens[int(str(i)[0])]) + " hundred and " + str(tens[int(str(i)[1]) - 2]) + " " + str(
+            singles_and_teens[int(str(i)[2])]))
 
+
+while True:
+    try:
+        num_to_word(int(input("Enter a number between 1 and 999: ")))
+    except:
+        print("Invalid Input")
