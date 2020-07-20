1148fdd9e8cab039291aaf6dd6aae7bd9922cf4d
Michael Hotaling <37930981+MichaelHotaling@users.noreply.github.com>
2020-07-20 13:45:28 -0400

    Assignment 8.1
    Added a function for csv format
    Added the option for the user to immediately open the file they created for ease of access

--- HOTALING_DSC510/wordCounterFileOutput.py
+++ HOTALING_DSC510/wordCounterFileOutput.py
@@ -2,9 +2,11 @@
 # Week 8
 # Programming Assignment Week 8
 # Author: Michael Hotaling
-# 07/06/2020
+# 07/20/2020
 
 import string
+import os
+import webbrowser
 
 
 def add_words(words, word_dictionary):
@@ -32,27 +34,58 @@ def process_line(line, word_dictionary):
     add_words(words, word_dictionary)
 
 
-def process_file(word_dictionary, output_filename):
+def process_file(word_dictionary, output_filename, title=""):
     output = open(output_filename, "w")
     # output.write("Length of the dictionary: " + str(len(word_dictionary) + "\n"))
-    output.write("-" * 34 + "\n")
-    output.write("|{:<25} {:5} |".format("Word", "Count") + "\n")
-    output.write("|" + ("-" * 32) + "|" + "\n")
+    output.write("{:^35}\n".format(title + ": " + str(len(word_dictionary)) + " unique words"))
+    output.write("{:-^35}\n".format("-"))
+    output.write("| {:>5} | {:<15}|{:>7} |\n".format("Index", "Word", "Count"))
+    output.write("{:-^35}\n".format("-"))
+    index = 1
     for key in sorted(word_dictionary, key=word_dictionary.get, reverse=True):
-        output.write("|{:<15} {:15} |".format(key, word_dictionary[key]) + "\n")
-    output.write("-" * 34 + "\n")
-    output.close()
+        output.write("| {:>5} | {:<15}|{:>7} |\n".format(index, key, word_dictionary[key]))
+        index += 1
+    output.write("{:-^35}\n".format("-"))
+
+
+def csv_process_file(word_dictionary, output_filename, title=""):
+    output = open(output_filename, "w")
+    # output.write("Length of the dictionary: " + str(len(word_dictionary) + "\n"))
+    output.write("{:^35}\n".format(title + ": " + str(len(word_dictionary)) + " unique words"))
+    output.write("{},{},{}\n".format("Index", "Word", "Count"))
+    index = 1
+    for key in sorted(word_dictionary, key=word_dictionary.get, reverse=True):
+        output.write("{},{},{}\n".format(index, key, word_dictionary[key]))
+        index += 1
 
 
 def main():
-    gba_file = open('gettysburg.txt', 'r')
+    path = os.getcwd()
+    file = "gettysburg.txt"
+    # file = input("What is the file name?: ")
+    gba_file = open(file, 'r')
     output_filename = input("Enter the output file name: ")
     word_dictionary = dict()
     for line in gba_file:
         process_line(line, word_dictionary)
     gba_file.close()
-    output = open(output_filename, "w")
-    process_file(word_dictionary, output_filename)
+    format_finder = output_filename.split(".")
+    
+    # This block could probably be optimized
+    if output_filename[-4:] == ".csv":
+        print("Exporting file as {0}".format(format_finder[len(format_finder)-1]))
+        csv_process_file(word_dictionary, output_filename, title=file)
+    elif "." in output_filename:
+        print("Exporting file as {0}".format(format_finder[len(format_finder)-1]))
+        process_file(word_dictionary, output_filename, title=file)
+    else:
+        print("No file format selected. Defaulting to txt")
+        output_filename = output_filename+".txt"
+        process_file(word_dictionary, output_filename, title=file)
+        
+    print("{0} exported to {1}".format(output_filename, path))
+    if input("Would you like to open the file now? [y/n]: ").lower() == "y":
+        webbrowser.open(output_filename)
 
 
 if __name__ == "__main__":
