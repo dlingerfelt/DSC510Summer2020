a016420e325734dc63a7054618775fc6ea1b5534
Michael Hotaling <37930981+MichaelHotaling@users.noreply.github.com>
2020-07-22 08:55:48 -0400

    Assignment 8.1
    Added error handling if the gettysburg.txt file isn't in the same working directory

--- HOTALING_DSC510/wordCounterFileOutput.py
+++ HOTALING_DSC510/wordCounterFileOutput.py
@@ -2,11 +2,11 @@
 # Week 8
 # Programming Assignment Week 8
 # Author: Michael Hotaling
-# 07/20/2020
+# 07/21/2020
 
 import string
-import os
-import webbrowser
+import os  # To print out the directory of the file
+import webbrowser  # to open the file once it is created
 
 
 def add_words(words, word_dictionary):
@@ -29,14 +29,13 @@ def process_line(line, word_dictionary):
     # We can use translate to covert the special characters to spaces
     line = line.translate(line.maketrans("", "", string.punctuation))
     # Finally, we can split all the words in each line using spaces as the delimiter
-    words = line.split(" ")
+    words = line.split()
     # We can then call the process_add_words function to add the words to the dictionary
     add_words(words, word_dictionary)
 
 
 def process_file(word_dictionary, output_filename, title=""):
     output = open(output_filename, "w")
-    # output.write("Length of the dictionary: " + str(len(word_dictionary) + "\n"))
     output.write("{:^35}\n".format(title + ": " + str(len(word_dictionary)) + " unique words"))
     output.write("{:-^35}\n".format("-"))
     output.write("| {:>5} | {:<15}|{:>7} |\n".format("Index", "Word", "Count"))
@@ -63,29 +62,47 @@ def main():
     path = os.getcwd()
     file = "gettysburg.txt"
     # file = input("What is the file name?: ")
-    gba_file = open(file, 'r')
-    output_filename = input("Enter the output file name: ")
+
+    try:
+        gba_file = open(file, 'r')
+    except FileNotFoundError as error:
+        print(error)
+        print("Please make sure the file is in the correct directory before running.")
+        exit()
+
+    output_filename = input("txt and csv formats are recommended\nEnter the output file name: ")
+    if os.path.exists(output_filename):
+        if input("There is already a file with that name. \nWould you like to overwrite? [y/n]: ").lower() != "y":
+            exit()
     word_dictionary = dict()
     for line in gba_file:
         process_line(line, word_dictionary)
     gba_file.close()
     format_finder = output_filename.split(".")
-    
+    format_finder = format_finder[len(format_finder)-1]
+
     # This block could probably be optimized
-    if output_filename[-4:] == ".csv":
-        print("Exporting file as {0}".format(format_finder[len(format_finder)-1]))
+    if format_finder == "csv":
+        # Special condition for CSV formats
+        # The will use the special function to export the file with comma separation
+        print("Exporting File as {0}".format(format_finder.upper()))
         csv_process_file(word_dictionary, output_filename, title=file)
     elif "." in output_filename:
-        print("Exporting file as {0}".format(format_finder[len(format_finder)-1]))
+        print("Exporting File as {0}".format(format_finder.upper()))
         process_file(word_dictionary, output_filename, title=file)
     else:
-        print("No file format selected. Defaulting to txt")
+        print("No File Format Selected. Defaulting to TXT")
         output_filename = output_filename+".txt"
         process_file(word_dictionary, output_filename, title=file)
-        
-    print("{0} exported to {1}".format(output_filename, path))
+
+    print("{0} Exported to {1}".format(output_filename, path))
     if input("Would you like to open the file now? [y/n]: ").lower() == "y":
+        print("Opening File...")
         webbrowser.open(output_filename)
+    if input("Would you like to delete the file now? [y/n]: ").lower() == "y":
+        print("Deleting File...")
+        os.remove(output_filename)
+        print("File Deleted")
 
 
 if __name__ == "__main__":
