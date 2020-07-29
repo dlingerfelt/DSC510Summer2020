d5f4ee4c1eced9c31271f2fedb2125b12ce04ce2
Michael Hotaling <37930981+MichaelHotaling@users.noreply.github.com>
2020-07-29 09:48:29 -0400

    Assignment 9.1
    Added comments

--- HOTALING_DSC510/chuckNorris.py
+++ HOTALING_DSC510/chuckNorris.py
@@ -12,20 +12,22 @@ import webbrowser
 
 def chuck_norris_joke(category):
     try:
-        # response = requests.get('https://api.chucknorris.io/jokes/random?category=dev', verify=False)  # My SSL
-        # certificates are all messed up so I need to disable verification
+        # I need an if statement to pull the correct base url if the user wants a specific category
         if category == "random":
             api_response = requests.get('http://api.chucknorris.io/jokes/random', verify=False)
         else:
             api_response = requests.get('http://api.chucknorris.io/jokes/random?category=' + category, verify=False)
         json_response = api_response.json()
         return json_response.get('value')
+
+        # Raise an error if the API rejects the query
     except requests.HTTPError:
         print("An HTTP Error has occurred. Please try again later.")
         exit()
 
 
 def category_puller():
+    # This pulls the categories from the API. We only need to pull this once.
     categories = requests.get('http://api.chucknorris.io/jokes/categories', verify=False).json()
     return categories
 
@@ -43,7 +45,7 @@ def joke_selector(categories):
         category_number_list.append(str(category_numbers))
         category_numbers += 1
 
-    # Random is also a category, but it isn't listed in the list. So let's just add it.
+    # Random is also a category, but it isn't listed in the API list. So let's just add it.
     # This was previously running each time the list was pulled, so I set it up to check whether or not random is
     # already in the list
     if 'random' not in categories:
@@ -53,6 +55,10 @@ def joke_selector(categories):
     print("-" * 49)
     # print(categories)
     # print(category_number_list)
+
+    # Now we can ask the user what kind of joke they want. If they input the type of category, we will just return it.
+    # If they input a numerical value, we can index the list.
+    # If it's not in either, we will return "Invalid request"
     while True:
         joke_selection = input("What kind of joke would you like to hear?: ").lower()
         if joke_selection in categories:
@@ -64,6 +70,7 @@ def joke_selector(categories):
 
 
 def create_output(joke_list):
+    # This is a function I made to export the jokes. Pretty much entirely lifted from last weeks assignment
     file_name = "Chuck_Norris_Jokes.txt"
     output = open(file_name, "w")
     output.write("Chuck Norris Jokes\n")
@@ -78,7 +85,9 @@ def create_output(joke_list):
 
 def main():
     # The main function is a bit long
+    # Greeting
     print("Welcome to the Chuck Norris Joke Machine")
+    # Pull the categories
     categories = category_puller()
     x = 1
     z = 0
